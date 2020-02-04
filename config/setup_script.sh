87ec7bae2dbd7e5ccf732e9d46374f92e5b4c4b3
Audrow <audrow.nash@gmail.com>
2020-02-04 08:00:06 -0800

    Make setup_script have more convenient command aliases

--- config/setup_script.sh
+++ config/setup_script.sh
@@ -5,4 +5,11 @@ echo 'Hello, master'
 source /opt/ros/kinetic/setup.bash 
 source /root/ws/catkin_ws/devel/setup.bash
 
-alias code="code --user-data-dir ~/.code"
+alias gs="git status"
+alias gl="git log --pretty=oneline --graph"
+alias gb="git branch -a"
+alias gg="git gui"
+alias rh="roscd; cd .."
+alias rs="roscd; cd ../src"
+alias cm="roscd; cd ..; catkin_make"
+alias code="code --user-data-dir /root/.visual_code/"
