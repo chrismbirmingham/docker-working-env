10794dbb8b2c24a74590c80c93b8e2d1de1cfbcb
Audrow <audrow.nash@gmail.com>
2020-04-01 08:14:15 -0700

    Allow run.sh to call different Dockerfiles

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -z ${1+x} ]; 
+then 
+	DOCKERFILE="ros1";
+else
+	DOCKERFILE=$1;
+fi
+
 # Go to the directory of this file -- needed for docker-compose
 FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $FILE_DIR
@@ -9,4 +16,4 @@ xhost +local:
 
 # build the docker container and run the default command 
 # set in the Dockerfile or docker-compose.yaml file
-ROS_MASTER_URI=$ROS_MASTER_URI docker-compose up --build
+DOCKERFILE=$DOCKERFILE ROS_MASTER_URI=$ROS_MASTER_URI docker-compose up --build
