df1c44a9245223d01a32d52e7b4893b5c392b66c
Chris <cmbirmingham19@gmail.com>
2020-05-28 15:03:45 -0700

    Clean up

--- build_devs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build --rm -f dockerfiles/dev/ubuntu16-dev --tag cmbirmingham/ubuntu16-dev:latest --squash .
-docker build --rm -f dockerfiles/dev/ros-kinetic-dev --tag cmbirmingham/ros-kinetic-dev:latest --squash .
-docker build --rm -f dockerfiles/dev/harmoni-dev --tag cmbirmingham/harmoni-dev:latest --squash .
-docker build --rm -f dockerfiles/dev/w2l-dev --tag cmbirmingham/w2l-dev:latest --squash .
-
