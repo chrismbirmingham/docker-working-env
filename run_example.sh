7f6a7742dfb061f7974708e120984e77796ed0c9
Chris <cmbirmingham19@gmail.com>
2020-06-05 12:48:13 -0700

    add gpu support

--- run_example.sh
+++ run_example.sh
@@ -10,13 +10,15 @@ xhost +local:
 # Optional methods for passing in args to docker compose
 if [ -z ${1+x} ]; 
 then 
-	WORKSPACE="path/to/projectdir";
+	WORKSPACE="path/to/project_dir";
 else
 	WORKSPACE=$1;
 fi
 
+# If you would like to run just a single dockerfile
 DOCKERFILE="folder/Dockerfile"
 
+# Set up docker container to run as user (so it doesn't create root files in the home dir)
 CURRENT_UID=$(id -u):$(id -g)
 
 PROJECT=$WORKSPACE DOCKERFILE=$DOCKERFILE CURRENT_UID=$CURRENT_UID docker-compose -f docker-compose.yml up
